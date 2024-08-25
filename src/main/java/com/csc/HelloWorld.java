16a39c7f59f4276df84545e9a276e8d351226c5f
Akshat Thakkar <65828988+Akshat1506-max@users.noreply.github.com>
2024-08-25 00:09:41 -0500

    New Hello World Code commit

--- src/main/java/com/csc/HelloWorld.java
+++ src/main/java/com/csc/HelloWorld.java
@@ -1,3 +1,10 @@
 package com.csc;
 
 // Your code goes here!
+class Test
+{
+    public static void main(String []args)
+    {
+        System.out.println("Hello World, this is my First Java Program.");
+    }
+};
